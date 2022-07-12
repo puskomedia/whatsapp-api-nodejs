f1071abf49d32149b2916e2caadf4d8f7d9dbb00
rochman25 <zaenur.rochman98@gmail.com>
2022-07-12 09:58:49 +0700

    fix undefined image file

--- src/message_broker/ListenerImage.js
+++ src/message_broker/ListenerImage.js
@@ -10,30 +10,35 @@ class ListenerImage {
                 type,
                 webhookUrl
             } = JSON.parse(messages.content.toString());
-            file.buffer = Buffer.from(file.buffer.data);
-            // console.log(id,file);
-            const data = await WhatsAppInstances[key].sendMediaFile(
-                id,
-                file,
-                type,
-                caption
-            )
-            if(data.status == 1){
-                if (typeof webhookUrl !== 'undefined') {
-                    if(webhookUrl != ''){
-                        const axiosInstance = axios.create({
-                            baseURL: webhookUrl,
-                        });
-                        const notifData = {
-                            key,
-                            message:caption,
-                            messageType: 'IMAGE',
-                            receiver: id,
-                            status: 2,
-                        };
-                        await axiosInstance.post('/api/set_status_message', notifData).catch(function(error){
-                            console.log(error.response)
-                        });
+            // eslint-disable-next-line no-empty
+            if(file.buffer === 'undefined'){
+                console.log('image undefined. please send it again.');
+            }else{
+                file.buffer = Buffer.from(file.buffer.data);
+                // console.log(id,file);
+                const data = await WhatsAppInstances[key].sendMediaFile(
+                    id,
+                    file,
+                    type,
+                    caption
+                )
+                if(data.status == 1){
+                    if (typeof webhookUrl !== 'undefined') {
+                        if(webhookUrl != ''){
+                            const axiosInstance = axios.create({
+                                baseURL: webhookUrl,
+                            });
+                            const notifData = {
+                                key,
+                                message:caption,
+                                messageType: 'IMAGE',
+                                receiver: id,
+                                status: 2,
+                            };
+                            await axiosInstance.post('/api/set_status_message', notifData).catch(function(error){
+                                console.log(error.response)
+                            });
+                        }
                     }
                 }
             }
